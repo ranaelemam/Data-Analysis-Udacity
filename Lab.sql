1101ac2e8cbb9246824b4bfa5ce566b7ec78f54f
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-05-07 22:59:51 +0300

    Update Lab.sql

--- Lab.sql
+++ Lab.sql
@@ -1,3 +1,21 @@
 SELECT invoice.*
 FROM invoice
 where invoice.invoicedate between '2013-01-01' and '2014-01-01';
+
+SELECT firstName,lastName,country
+ FROM customer
+ WHERE country IN("Brazil","Canada","India","Sweden");
+ 
+   
+SELECT Track.name, Track.Composer, Album.Title 
+FROM Track
+JOIN Album
+ON Track.AlbumId = Album.AlbumId
+WHERE Track.name LIKE 'A%' AND Track.Composer NOT LIKE '';
+
+SELECT C.Firstname,C.Lastname,I.Total
+FROM Customer AS C
+JOIN Invoice AS I
+ON C.CustomerId =I.CustomerId
+ORDER BY I.total DESC
+LIMIT 10
