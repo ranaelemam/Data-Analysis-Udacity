c3d8389a82e98dbb2e7dce3a666147554b3d9ea5
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-05-07 23:48:04 +0300

    Update Quizez.sql

--- Quizez.sql
+++ Quizez.sql
@@ -37,6 +37,7 @@ FROM web_events w
 JOIN accounts a
 ON w.account_id = a.id
 WHERE a.name = 'Walmart';
+
 SELECT r.name region, s.name rep, a.name account
 FROM sales_reps s
 JOIN region r
@@ -44,6 +45,7 @@ ON s.region_id = r.id
 JOIN accounts a
 ON a.sales_rep_id = s.id
 ORDER BY a.name;
+
 SELECT r.name region, a.name account, 
        o.total_amt_usd/(o.total + 0.01) unit_price
 FROM region r
@@ -58,10 +60,12 @@ SELECT id, occurred_at, total_amt_usd
 FROM orders
 ORDER BY occurred_at
 LIMIT 10;
+
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY total_amt_usd DESC 
 LIMIT 5;
+
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY total_amt_usd
@@ -70,6 +74,7 @@ LIMIT 20;
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY account_id, total_amt_usd DESC;
+
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY total_amt_usd DESC, account_id;
@@ -78,6 +83,7 @@ SELECT *
 FROM orders
 WHERE gloss_amt_usd >= 1000
 LIMIT 5;
+
 SELECT *
 FROM orders
 WHERE total_amt_usd < 500
@@ -86,6 +92,7 @@ LIMIT 10;
 SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price
 FROM orders
 LIMIT 10;
+
 SELECT id, account_id, 
    poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
 FROM orders
@@ -94,26 +101,23 @@ LIMIT 10;
 SELECT name
 FROM accounts
 WHERE name LIKE 'C%';
+
 SELECT name
 FROM accounts
 WHERE name LIKE '%one%';
+
 SELECT name
 FROM accounts
 WHERE name LIKE '%s';
 
-SELECT name, primary_poc, sales_rep_id
-FROM accounts
-WHERE name IN ('Walmart', 'Target', 'Nordstrom');
-SELECT *
-FROM web_events
-WHERE channel IN ('organic', 'adwords');
-
 SELECT name
 FROM accounts
 WHERE name NOT LIKE 'C%';
+
 SELECT name
 FROM accounts
 WHERE name NOT LIKE '%one%';
+
 SELECT name
 FROM accounts
 WHERE name NOT LIKE '%s';
@@ -121,12 +125,15 @@ WHERE name NOT LIKE '%s';
 SELECT *
 FROM orders
 WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
 SELECT name
 FROM accounts
 WHERE name NOT LIKE 'C%' AND name LIKE '%s';
+
 SELECT occurred_at, gloss_qty 
 FROM orders
 WHERE gloss_qty BETWEEN 24 AND 29;
+
 SELECT *
 FROM web_events
 WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
@@ -136,9 +143,11 @@ ORDER BY occurred_at DESC;
 SELECT id
 FROM orders
 WHERE gloss_qty > 4000 OR poster_qty > 4000;
+
 SELECT *
 FROM orders
 WHERE standard_qty = 0 AND (gloss_qty > 1000 OR poster_qty > 1000);
+
 SELECT *
 FROM accounts
 WHERE (name LIKE 'C%' OR name LIKE 'W%') 
@@ -155,6 +164,7 @@ JOIN accounts a
 ON a.sales_rep_id = s.id
 WHERE r.name = 'Midwest'
 ORDER BY a.name;
+
 SELECT r.name region, s.name rep, a.name account
 FROM sales_reps s
 JOIN region r
@@ -163,6 +173,7 @@ JOIN accounts a
 ON a.sales_rep_id = s.id
 WHERE r.name = 'Midwest' AND s.name LIKE 'S%'
 ORDER BY a.name;
+
 SELECT r.name region, s.name rep, a.name account
 FROM sales_reps s
 JOIN region r
@@ -171,6 +182,7 @@ JOIN accounts a
 ON a.sales_rep_id = s.id
 WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
 ORDER BY a.name;
+
 SELECT r.name region, a.name account, o.total_amt_usd/(o.total + 0.01) unit_price
 FROM region r
 JOIN sales_reps s
@@ -180,6 +192,7 @@ ON a.sales_rep_id = s.id
 JOIN orders o
 ON o.account_id = a.id
 WHERE o.standard_qty > 100;
+
 SELECT r.name region, a.name account, o.total_amt_usd/(o.total + 0.01) unit_price
 FROM region r
 JOIN sales_reps s
@@ -190,6 +203,7 @@ JOIN orders o
 ON o.account_id = a.id
 WHERE o.standard_qty > 100 AND o.poster_qty > 50
 ORDER BY unit_price;
+
 SELECT r.name region, a.name account, o.total_amt_usd/(o.total + 0.01) unit_price
 FROM region r
 JOIN sales_reps s
@@ -200,11 +214,13 @@ JOIN orders o
 ON o.account_id = a.id
 WHERE o.standard_qty > 100 AND o.poster_qty > 50
 ORDER BY unit_price DESC;
+
 SELECT DISTINCT a.name, w.channel
 FROM accounts a
 JOIN web_events w
 ON a.id = w.account_id
 WHERE a.id = '1001';
+
 SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
 FROM accounts a
 JOIN orders o
