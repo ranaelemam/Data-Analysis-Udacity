2d1d16cc473e87efd831d3892d78d76ed08d9394
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-05-11 00:24:15 +0300

    Update Quizzes-2.sql

--- Quizzes-2.sql
+++ Quizzes-2.sql
@@ -1,1 +1,10 @@
-
+SELECT SUM(poster_qty) AS total_poster_sales
+FROM orders;
+SELECT SUM(standard_qty) AS total_standard_sales
+FROM orders;
+SELECT SUM(total_amt_usd) AS total_dollar_sales
+FROM orders;
+SELECT standard_amt_usd + gloss_amt_usd AS total_standard_gloss
+FROM orders;
+SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS standard_price_per_unit
+FROM orders;
